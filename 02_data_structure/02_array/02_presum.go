c75523e65fe1b3d6cec49a97432bb60e3545b6d3
daijun <daijun@virtaitech.com>
2024-05-06 23:35:35 +0800

    presum

--- 02_data_structure/02_array/02_presum.go
+++ 02_data_structure/02_array/02_presum.go
@@ -34,3 +34,30 @@ func pivotIndex(nums []int) int {
 	}
 	return -1
 }
+
+// 除自身以外数组的乘积
+func productExceptSelf(nums []int) []int {
+	length := len(nums)
+
+	// 前缀积 [0...i]之间的乘积
+	prefix := make([]int, length)
+	prefix[0] = nums[0]
+	for i := 1; i < length; i++ {
+		prefix[i] = prefix[i-1] * nums[i]
+	}
+
+	// 后缀积 [i...length]之间的乘积
+	suffix := make([]int, length)
+	suffix[length-1] = nums[length-1]
+	for i := length - 2; i >= 0; i-- {
+		suffix[i] = suffix[i+1] * nums[i]
+	}
+
+	res := make([]int, length)
+	res[0] = suffix[1]
+	res[length-1] = prefix[length-2]
+	for i := 1; i < length-1; i++ {
+		res[i] = prefix[i-1] * suffix[i+1]
+	}
+	return res
+}
