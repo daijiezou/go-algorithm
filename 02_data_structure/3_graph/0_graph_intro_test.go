f710fe0330ba9139b8a7dbc9f419ab2aa6cde51a
daijun <daijun@virtaitech.com>
2024-07-04 10:03:27 +0800

    graph

--- 02_data_structure/3_graph/0_graph_intro_test.go
+++ 02_data_structure/3_graph/0_graph_intro_test.go
@@ -58,3 +58,30 @@ func Test_canFinish(t *testing.T) {
 		})
 	}
 }
+
+func Test_canFinish1(t *testing.T) {
+	type args struct {
+		numCourses    int
+		prerequisites [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "case1",
+			args: args{
+				numCourses:    20,
+				prerequisites: [][]int{[0,10],[3,18],[5,5],[6,11],[11,14],[13,1],[15,1],[17,4]},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canFinish(tt.args.numCourses, tt.args.prerequisites); got != tt.want {
+				t.Errorf("canFinish() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
