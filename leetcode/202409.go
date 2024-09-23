a302abaf0ea033e89554f8e5ed72e3906e78039f
daijun <daijun@virtaitech.com>
2024-09-23 11:27:39 +0800

    每日一题

--- leetcode/202409.go
+++ leetcode/202409.go
@@ -438,3 +438,35 @@ func findJudge(n int, trust [][]int) int {
 	}
 	return -1
 }
+
+/*
+给你一个正整数数组 values，其中 values[i] 表示第 i 个观光景点的评分，并且两个景点 i 和 j 之间的 距离 为 j - i。
+一对景点（i < j）组成的观光组合的得分为 values[i] + values[j] + i - j ，
+也就是景点的评分之和 减去 它们两者之间的距离。
+返回一对观光景点能取得的最高分。
+*/
+// https://leetcode.cn/problems/best-sightseeing-pair/description/
+func maxScoreSightseeingPair(values []int) int {
+
+	n := len(values)
+	res := 0
+	for i := 0; i < n; i++ {
+		jMax := min(n, i+values[i]+1)
+		for j := i + 1; j < jMax; j++ {
+			cur := values[i] + i + values[j] - j
+			res = max(res, cur)
+		}
+
+	}
+	return res
+}
+
+func OfficialMaxScoreSightseeingPair(values []int) int {
+	ans, mx := 0, values[0]+0
+	for j := 1; j < len(values); j++ {
+		ans = max(ans, mx+values[j]-j)
+		// 边遍历边维护
+		mx = max(mx, values[j]+j)
+	}
+	return ans
+}
--- leetcode/202409_test.go
+++ leetcode/202409_test.go
@@ -105,3 +105,36 @@ func Test_latestTimeCatchTheBus(t *testing.T) {
 		})
 	}
 }
+
+func Test_maxScoreSightseeingPair(t *testing.T) {
+	type args struct {
+		values []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				values: []int{8, 1, 5, 2, 6},
+			},
+			want: 11,
+		},
+		{
+			name: "case 2",
+			args: args{
+				values: []int{1, 2},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxScoreSightseeingPair(tt.args.values); got != tt.want {
+				t.Errorf("maxScoreSightseeingPair() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
